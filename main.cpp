e8382eabec17dc1f2e6c5e4eb9762e71aa036895
Анатолій <t0663261690@gmail.com>
2024-12-07 21:25:30 +0200

    void

--- main.cpp
+++ main.cpp
@@ -1,35 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
-#include <conio.h> // äëÿ getche()
+float lbstokg(float);
+
 int main()
 {
-	char dir = 'a';
-	int x = 10, y = 10;
-	cout << "Натисніть Enter для виходу...\n";
-	while (dir != '\r ') // пока не нажата Enter
-	{
-		cout << "\nВаші координати: " << x << ", " << y;
-		cout << "\nВиберіть направлення (n,s,e,w): ";
-		dir = _getche(); 
-		if (dir == 'n') // на север
-			y--;
-		else
-			if (dir == 's') // на юг
-				y++;
-			else
-				if (dir == 'e') // на восток
-					x++;
-				else
-					if (dir == 'w')
-					{
-						x--;
-	
-					}
-						
-	} 
-	cout << "ви заблукали" << endl;
-
-	
+	float lbs, kgs;
+	cout << "\nВведіть вес в фунтах - ";
+	cin >> lbs;
+	kgs = lbstokg(lbs);
+	cout << "Вага в кілограмах равна - " << kgs << endl;
 
 	return 0;
+}
+
+float lbstokg(float pounds) {
+	float kilograms = 0.45392 * pounds;
+	return kilograms;
 }
